48738a9abc004c0e8e711ff7e49225f9f1ed26ee
Diluxshana-2001 <diluxshana6@gmail.com>
2024-12-29 22:28:14 +0530

    added id and placeholder for the fields

--- frontend/src/components/AddAppoinment.jsx
+++ frontend/src/components/AddAppoinment.jsx
@@ -7,27 +7,27 @@ export default function AddAppointment(){
         <form className="">
             <div className=''>
                 <label className=''>Patient Registration Number </label>
-                <input type='text' className='' value='' id='' placeholder='' ></input>
+                <input type='text' className='' value='' id='regno' placeholder='Enter Registration no' ></input>
             </div>
             <div className=''>
                 <label className=''>Patient Name </label>
-                <input type='text' className='' value='' id='' placeholder='' ></input>
+                <input type='text' className='' value='' id='name' placeholder='Enter name' ></input>
             </div>
             <div className=''>
                 <label className=''>Email </label>
-                <input type='email' className='' value='' id='' placeholder='' ></input>
+                <input type='email' className='' value='' id='email' placeholder='Enter email' ></input>
             </div>
             <div className=''>
                 <label className=''>Date </label>
-                <input type='date' className='' value='' id='' placeholder='' ></input>
+                <input type='date' className='' value='' id='date' ></input>
             </div>
             <div className=''>
                 <label className=''>Time </label>
-                <input type='time' className='' value='' id='' placeholder='' ></input>
+                <input type='time' className='' value='' id='time' ></input>
             </div>
             <div className=''>
                 <label className=''>Condition </label>
-                <input type='text' className='' value='' id='' placeholder='' ></input>
+                <input type='text' className='' value='' id='condition' placeholder='Enter condition' ></input>
             </div>
             <button type='submit'>ADD</button>
         </form>
