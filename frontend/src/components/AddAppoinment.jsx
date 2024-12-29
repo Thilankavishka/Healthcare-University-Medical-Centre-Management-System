5fe2d996f62182767cc2916935381defe5294e46
Diluxshana-2001 <diluxshana6@gmail.com>
2024-12-29 22:10:12 +0530

    Added the form for patient appoinmnet registration with the field patient Name, regno,email,date,time, and condition

--- frontend/src/components/AddAppoinment.jsx
+++ frontend/src/components/AddAppoinment.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
 export default function AddAppointment(){
-    <div>
-        
-    </div>
+    return(
+        <>
+        <div className=''></div>
+        <form className="">
+            <div className=''>
+                <label className=''>Patient Registration Number </label>
+                <input type='text' className='' value='' id='' placeholder='' ></input>
+            </div>
+            <div className=''>
+                <label className=''>Patient Name </label>
+                <input type='text' className='' value='' id='' placeholder='' ></input>
+            </div>
+            <div className=''>
+                <label className=''>Email </label>
+                <input type='email' className='' value='' id='' placeholder='' ></input>
+            </div>
+            <div className=''>
+                <label className=''>Date </label>
+                <input type='date' className='' value='' id='' placeholder='' ></input>
+            </div>
+            <div className=''>
+                <label className=''>Time </label>
+                <input type='time' className='' value='' id='' placeholder='' ></input>
+            </div>
+            <div className=''>
+                <label className=''>Condition </label>
+                <input type='text' className='' value='' id='' placeholder='' ></input>
+            </div>
+            <button type='submit'>ADD</button>
+        </form>
+        </>
+    )
 }
