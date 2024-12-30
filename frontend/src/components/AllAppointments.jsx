3f83a4f2bb2845f0accfb6b8a9dacb1a5069cc13
Diluxshana-2001 <diluxshana6@gmail.com>
2024-12-30 10:33:04 +0530

    created a table to show the appoinments

--- frontend/src/components/AllAppointments.jsx
+++ frontend/src/components/AllAppointments.jsx
@@ -2,7 +2,7 @@ export default function AllAppointments(){
     return(
         <>
         <div className="">
-            <h1 className="">
+            <h1 className="">All Appointments</h1>
                 <div className="">
                     <button>Add Appointments</button>
                 </div>
@@ -10,9 +10,19 @@ export default function AllAppointments(){
                 <div className="">
                     <input type="text" placeholder="Search by Patient Name" value={searchterm}></input>
                 </div>
-            </h1>
-            
         </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Patient Registration number</th>
+                    <th>Patient Name</th>
+                    <th>Email</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>Condition</th>
+                </tr>
+            </thead>
+        </table>
         </>
     )
 }
