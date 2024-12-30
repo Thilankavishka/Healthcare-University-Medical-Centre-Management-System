eb3b6175db72f580a31ec282b0b7a52541b7c4a4
Diluxshana-2001 <diluxshana6@gmail.com>
2024-12-30 09:06:34 +0530

    added some styles for the labels in the add apoinment page

--- frontend/src/components/AddAppoinment.jsx
+++ frontend/src/components/AddAppoinment.jsx
@@ -3,31 +3,31 @@ import React from 'react';
 export default function AddAppointment(){
     return(
         <>
-        <div className=''></div>
-        <form className="">
+        <div className='container'></div>
+        <form className="form">
             <div className=''>
-                <label className=''>Patient Registration Number </label>
-                <input type='text' className='' value='' id='regno' placeholder='Enter Registration no' ></input>
+                <label className='Form-label'>Patient Registration Number </label>
+                <input type='text' className='form-control' value='' id='regno' placeholder='Enter Registration no' ></input>
             </div>
             <div className=''>
-                <label className=''>Patient Name </label>
-                <input type='text' className='' value='' id='name' placeholder='Enter name' ></input>
+                <label className='Form-label'>Patient Name </label>
+                <input type='text' className='form-control' value='' id='name' placeholder='Enter name' ></input>
             </div>
             <div className=''>
-                <label className=''>Email </label>
-                <input type='email' className='' value='' id='email' placeholder='Enter email' ></input>
+                <label className='Form-label'>Email </label>
+                <input type='email' className='form-control' value='' id='email' placeholder='Enter email' ></input>
             </div>
             <div className=''>
-                <label className=''>Date </label>
-                <input type='date' className='' value='' id='date' ></input>
+                <label className='Form-label'>Date </label>
+                <input type='date' className='form-control' value='' id='date' ></input>
             </div>
             <div className=''>
-                <label className=''>Time </label>
-                <input type='time' className='' value='' id='time' ></input>
+                <label className='Form-label'>Time </label>
+                <input type='time' className='form-control' value='' id='time' ></input>
             </div>
             <div className=''>
-                <label className=''>Condition </label>
-                <input type='text' className='' value='' id='condition' placeholder='Enter condition' ></input>
+                <label className='Form-label'>Condition </label>
+                <input type='text' className='form-control' value='' id='condition' placeholder='Enter condition' ></input>
             </div>
             <button type='submit'>ADD</button>
         </form>
