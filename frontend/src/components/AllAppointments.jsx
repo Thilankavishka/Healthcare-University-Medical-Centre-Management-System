dad3f9b0c2e84356ad217644ad091f3a5df830b4
Diluxshana-2001 <diluxshana6@gmail.com>
2024-12-30 11:03:52 +0530

    mpped through all appointments and render details in the table

--- frontend/src/components/AllAppointments.jsx
+++ frontend/src/components/AllAppointments.jsx
@@ -1,29 +1,43 @@
 export default function AllAppointments(){
     return(
         <>
-        <div className="">
-            <h1 className="">All Appointments</h1>
-                <div className="">
-                    <button>Add Appointments</button>
-                </div>
-                <br></br>
-                <div className="">
-                    <input type="text" placeholder="Search by Patient Name" value={searchterm}></input>
-                </div>
+        <div className="container">
+            <h1>All Appointments</h1>
+                <div className="container">
+                        <div>
+                            <button>Add Appointments</button>
+                        </div>
+                        <br></br>
+                        <div className="serach-container">
+                            <input type="text" placeholder="Search by Patient Name" value={searchterm}></input>
+                        </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Patient Registration number</th>
+                            <th>Patient Name</th>
+                            <th>Email</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Condition</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {appoinments.map(appointment => (
+                            <tr key={appointment._id}>
+                            <td>{appointment.regno}</td>
+                            <td>{appointment.pname}</td>
+                            <td>{appointment.email}</td>
+                            <td>{appointment.regno}</td>
+                            <td>{appointment.date}</td>
+                            <td>{appointment.time}</td>
+                            <td>{appointment.condition}</td>
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <table>
-            <thead>
-                <tr>
-                    <th>Patient Registration number</th>
-                    <th>Patient Name</th>
-                    <th>Email</th>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th>Condition</th>
-                </tr>
-            </thead>
-            <tbody></tbody>
-        </table>
         </>
     )
 }
