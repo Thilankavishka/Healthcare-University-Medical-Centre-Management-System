bf2876e06859e3a24105d7be1c89a904a5805db8
Diluxshana-2001 <diluxshana6@gmail.com>
2024-12-30 09:11:24 +0530

    added valuesto the fields regno,pname,email,time,date and condition

--- frontend/src/components/AddAppoinment.jsx
+++ frontend/src/components/AddAppoinment.jsx
@@ -7,27 +7,27 @@ export default function AddAppointment(){
         <form className="form">
             <div className=''>
                 <label className='Form-label'>Patient Registration Number </label>
-                <input type='text' className='form-control' value='' id='regno' placeholder='Enter Registration no' ></input>
+                <input type='text' className='form-control' value={regno} id='regno' placeholder='Enter Registration no' ></input>
             </div>
             <div className=''>
                 <label className='Form-label'>Patient Name </label>
-                <input type='text' className='form-control' value='' id='name' placeholder='Enter name' ></input>
+                <input type='text' className='form-control' value={pname} id='name' placeholder='Enter name' ></input>
             </div>
             <div className=''>
                 <label className='Form-label'>Email </label>
-                <input type='email' className='form-control' value='' id='email' placeholder='Enter email' ></input>
+                <input type='email' className='form-control' value={email} id='email' placeholder='Enter email' ></input>
             </div>
             <div className=''>
                 <label className='Form-label'>Date </label>
-                <input type='date' className='form-control' value='' id='date' ></input>
+                <input type='date' className='form-control' value={date} id='date' ></input>
             </div>
             <div className=''>
                 <label className='Form-label'>Time </label>
-                <input type='time' className='form-control' value='' id='time' ></input>
+                <input type='time' className='form-control' value={time} id='time' ></input>
             </div>
             <div className=''>
                 <label className='Form-label'>Condition </label>
-                <input type='text' className='form-control' value='' id='condition' placeholder='Enter condition' ></input>
+                <input type='text' className='form-control' value={condition} id='condition' placeholder='Enter condition' ></input>
             </div>
             <button type='submit'>ADD</button>
         </form>
