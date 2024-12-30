586c1ea17ce0aee364320557873058bb3d9a30fb
Diluxshana-2001 <diluxshana6@gmail.com>
2024-12-30 10:29:48 +0530

    created a button for add apointments and  added search bar for serach the patient details in Allapointments page

--- frontend/src/components/AllAppointments.jsx
+++ frontend/src/components/AllAppointments.jsx
@@ -1,3 +1,18 @@
 export default function AllAppointments(){
-
+    return(
+        <>
+        <div className="">
+            <h1 className="">
+                <div className="">
+                    <button>Add Appointments</button>
+                </div>
+                <br></br>
+                <div className="">
+                    <input type="text" placeholder="Search by Patient Name" value={searchterm}></input>
+                </div>
+            </h1>
+            
+        </div>
+        </>
+    )
 }
