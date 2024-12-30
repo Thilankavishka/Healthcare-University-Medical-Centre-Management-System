a824d3948394700e39cb082b989afb4bb91af23a
TharuShiMun <tharushimunasinghe516@gmail.com>
2024-12-30 19:57:39 +0530

    Define Medicalhistory component and initialize state

--- frontend/src/components/Medicalhistory.jsx
+++ frontend/src/components/Medicalhistory.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/App.css';
 import { useNavigate } from 'react-router-dom';
 
  function Medicalhistory(){
+    const navigate = useNavigate();
+    const [medicalData, setMedicalData]=useState([]);
     return(
         <>
         
