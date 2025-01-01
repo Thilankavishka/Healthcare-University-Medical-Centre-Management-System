282cd26dd2f93a714dd61254acea3708bcdf8482
Diluxshana-2001 <diluxshana6@gmail.com>
2025-01-01 21:31:48 +0530

    Created a save() function to to the appointment in the database and added success or failure message.

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -1,17 +1,26 @@
-exports.create = (req,res)=>{
-    if(!req.body){
-        res.status(400).send({message: "Content cannot be empty"});
+exports.create = (req, res) => {
+    if (!req.body) {
+        res.status(400).send({ message: "Content cannot be empty" });
         return;
     }
+
+
+    const { regno, pname, email, date, time, condition } = req.body;
+
+    const appointment = new Appointment({
+        regno,
+        pname,
+        email,
+        date,
+        time,
+        condition
+    });
+    //save appointment in the database
+    appointment.save()
+        .then(() => {
+            res.status(201).send({ message: "Appointment Registered successfully" });
+        })
+        .catch(err => {
+            res.status(201).send({ message: err.message || "Error occured while registering the appointment" });
+        });
 }
-
-const {regno, pname, email, date, time, condition } = req.body;
-
-const appointment = new Appointment({
-    regno,
-    pname,
-    email,
-    date,
-    time,
-    condition
-});
