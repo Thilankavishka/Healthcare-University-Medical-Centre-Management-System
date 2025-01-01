78bf596db1611fe631969f027262527f662d9974
Diluxshana-2001 <diluxshana6@gmail.com>
2025-01-01 21:48:44 +0530

    fixed route paths and connected them to the correct controller methods for handling create, get, all, and update appointments

--- backend/routes/appoinmentroute.js
+++ backend/routes/appoinmentroute.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const appointmentController = require('../controllers/appointmentController');
+
+router.post('./Appointments', appointmentController);
+
+router.get('./Appointments', appointmentController);
+
+router.put('/Appointments/:id',appointmentController);
+
