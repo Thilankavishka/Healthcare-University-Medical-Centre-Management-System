334b9c98aeb414ae4c0d79d78d5efe0f92f7ceb9
TharuShiMun <tharushimunasinghe516@gmail.com>
2025-01-02 06:48:11 +0530

    add necessary imports

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,6 +1,12 @@
+import React,{useState}  from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/App.css';
+
+
 export default function Form(){
     return(
         <>
+
         </>
     );
 }
