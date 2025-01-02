e74f85d37c61f06dfbf27afe5c7eae158be74562
Diluxshana-2001 <diluxshana6@gmail.com>
2025-01-02 21:05:56 +0530

    Added classname for appointment labels

--- frontend/src/components/AddAppoinment.jsx
+++ frontend/src/components/AddAppoinment.jsx
@@ -14,7 +14,7 @@ export default function AddAppointment(){
     const[time,setTime] = useState("");
     const[condition,setCondition] = useState("");
 
-    async function allAppointment(e){
+    async function addAppointment(e){
         e.preventDefault();
 
         const newAppointment = {
@@ -55,29 +55,31 @@ export default function AddAppointment(){
     return(
         <>
         <div className='container'>
-            <form className="form" onSubmit={allAppointment} >
-                <div className=''>
-                    <label className='Form-label'>Patient Registration Number </label>
+
+            <br />
+            <form className="my-form" onSubmit={addAppointment} >
+                <div className='mb-3'>
+                    <label forHtml="regno" className='Form-label'>Patient Registration Number </label>
                     <input type='text' className='form-control' value={regno} id='regno' placeholder='Enter Registration no' onChange={(e) => {setRegNo(e.target.value)}}></input>
                 </div>
-                <div className=''>
-                    <label className='Form-label'>Patient Name </label>
+                <div className='mb-3'>
+                    <label forHtml="pname" className='Form-label'>Patient Name </label>
                     <input type='text' className='form-control' value={pname} id='name' placeholder='Enter name' onChange={(e) => {setPatientName(e.target.value)}}></input>
                 </div>
-                <div className=''>
-                    <label className='Form-label'>Email </label>
+                <div className='mb-3'>
+                    <label forHtml="email" className='Form-label'>Email </label>
                     <input type='email' className='form-control' value={email} id='email' placeholder='Enter email' onChange={(e) => {setEmail(e.target.value)}}></input>
                 </div>
-                <div className=''>
-                    <label className='Form-label'>Date </label>
+                <div className='mb-3'>
+                    <label forHtml="date" className='Form-label'>Date </label>
                     <input type='date' className='form-control' value={date} min={getCurrentDate()} id='date' onChange={(e) => {setDate(e.target.value)}}></input>
                 </div>
-                <div className=''>
-                    <label className='Form-label'>Time </label>
+                <div className='mb-3'>
+                    <label forHtml="time" className='Form-label'>Time </label>
                     <input type='time' className='form-control' value={time} id='time' onChange={handleTimeChage}></input>
                 </div>
-                <div className=''>
-                    <label className='Form-label'>Condition </label>
+                <div className='mb-3'>
+                    <label forHtml="condition" className='Form-label'>Condition </label>
                     <textarea type='text' className='form-control' value={condition} id='condition' placeholder='Enter condition' onChange={(e) => {setCondition(e.target.value)}}></textarea>
                 </div>
                 <button type='submit'>ADD</button>
