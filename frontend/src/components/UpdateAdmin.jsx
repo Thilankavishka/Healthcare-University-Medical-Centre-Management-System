7f3aba7d3cc2c2f251d5d9f9802486add4027f28
piyumalBhathiya <fpiyumal450@gmail.com>
2025-03-29 13:24:48 +0530

    Bugfixing

--- frontend/src/components/UpdateAdmin.jsx
+++ frontend/src/components/UpdateAdmin.jsx
@@ -70,7 +70,7 @@ export default function UpdateAdmin() {
           </div>
           <div>
             <button
-              onClick={() => navigate("/admindashboard")} // Changed to admindashboard
+              onClick={() => navigate("/superadmindashboard")} // Changed to admindashboard
               className="w-full bg-white text-blue-600 py-2 px-4 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
             >
               Back to Dashboard
