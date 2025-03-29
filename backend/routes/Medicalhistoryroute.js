452c980bbe61e82b462939ab2696b915e710f617
TharuShiMun <tharushimunasinghe516@gmail.com>
2025-03-29 10:28:17 +0530

    set up medical history router and import required models

---
+++ backend/routes/Medicalhistoryroute.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const MedicalHistoryModel = require("../models/medicalhistory");
+const PatientModel = require("../models/patient");
+//const authMiddleware = require("../security/medicalhistory");
